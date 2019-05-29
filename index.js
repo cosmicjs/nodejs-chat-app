5ab5f507aa0d7de15e5a34f4932b8c71d21b50ec
JacobKnaack <jacobknck@gmail.com>
2019-05-29 15:23:05 -0700

    changing index and package configuration for deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const { renderToString } = require('react-dom/server');
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
   // Render our react application
